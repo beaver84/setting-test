f46662f33ee87c1cb5268afc640e9dcca8287942
baekyungtae <kyungtae.bae@timf.co.kr>
2023-03-11 12:36:33 +0900

    Initial commit

---
+++ src/test/java/com/example/settingtest/SettingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.settingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettingTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
